5267f5b82d70701b06f8225bd3bffb0e8efb3e64
estherayebaza <estherayebaza@gmail.com>
2022-10-21 14:47:52 +0300

    second commit

--- inventorySystem/inventory/urls.py
+++ inventorySystem/inventory/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import index
+from .views import inventory_list,per_product_view
 
 urlpatterns = [
-    path("",index,name="index")
-    
+    path("",inventory_list,name="inventory_list"),
+    path("per_product/<int:pk>", per_product_view,name="per_product")
+   
 ]
--- inventorySystem/inventory/views.py
+++ inventorySystem/inventory/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Inventory
 
 # Create your views here.
-def index(request):
+def inventory_list(request):
+    inventories=Inventory.objects.all()
     context={
-        "title":"Homepage"
+        "title":"Inventory list",
+        "inventories":inventories
     }
-    return render(request,"inventory/index.html",context=context)
+    return render(request,"inventory/inventory_list.html",context=context)
+def per_product_view(request, pk):
+    inventory = get_object_or_404(Inventory, pk=pk)
+    context = {
+        'inventory':inventory
+    }
+    return render(request, "inventory/per_product.html", context=context)
